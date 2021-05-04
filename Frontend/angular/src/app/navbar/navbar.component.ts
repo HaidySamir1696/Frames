953e6fae5133126842fe2b64d9a5f01eab29c767
HaidySamir1696 <haidysameer@hotmail.com>
2021-05-04 21:54:07 +0200

    update

--- Frontend/angular/src/app/navbar/navbar.component.ts
+++ Frontend/angular/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../interfaces/product';
+import { ProjectsService } from '../service/projects.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private projectsService:ProjectsService) { }
+ 
+  products:Product[]=[]
+  getProducts(){
+    this.projectsService.getProducts().subscribe((res)=>{
+      this.products = res
+    })
+  }
   ngOnInit(): void {
+    this.getProducts()
   }
 
 }
